7d532215e19a09734360763a0d5594dd70438cba
basit2023 <babaralibj362@gmail.com>
2024-12-20 18:06:27 +0500

    code updated

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -5,7 +5,7 @@ import ResponsiveImage from './ResponsiveImage';
 const AboutUs = () => {
   return (
     <>
-      <div className="relative w-full min-h-52 sm:h-80 md:h-96 lg:min-h-screen flex items-center justify-center overflow-hidden">
+      <div className="relative w-full min-h-48 sm:h-80 md:h-96 lg:min-h-screen flex items-center justify-center overflow-hidden">
         
         {/* Responsive background image */}
         <div className="absolute inset-0 z-0">
@@ -18,7 +18,7 @@ const AboutUs = () => {
 
         {/* Main content */}
         <div className="relative z-10 w-full px-4 sm:px-6 lg:px-8">
-          <div className="mt-[-10%] xs:ml-[20%] xs:mt-[-2%] sm:mt-0 ml-[28%] sm:ml-[25%] md:ml-[25%] lg:ml-[25%] w-[70%] sm:w-[50%] md:w-[60%] lg:w-[50%] xl:w-[43%] bg-white/80 backdrop-blur-sm rounded-lg pt-1 xs:pt-0 sm:p-6 lg:p-8 xl:p-8">
+          <div className=" xs:ml-[20%] xs:mt-[-2%] sm:mt-0 ml-[25%] sm:ml-[25%] md:ml-[25%] lg:ml-[25%] w-[70%] sm:w-[50%] md:w-[60%] lg:w-[50%] xl:w-[43%] bg-white/80 backdrop-blur-sm rounded-lg pt-1 xs:pt-0 sm:p-6 lg:p-8 xl:p-8">
             {/* About Us Title */}
             <h1 className=" text-base xs:text-lg sm:text-lg md:text-2xl lg:text-3xl font-bold text-left mb-0  sm:mb-1 font-gotham">
               <span className="text-[#FF000E]">About</span> <span className="text-black">us</span>
